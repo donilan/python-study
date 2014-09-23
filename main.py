4c747b0944ef0ff2c8bc3c651dcb3e0845e1b33e
Doni <d@ii2d.com>
2014-09-23 22:03:12 +0800

    Finish

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python
 
-if __name__ == '__main__':
+def add_properties(clazz):
+  clazz.a = 1
+  clazz.b = 2
+  clazz.c = 3
+
+class A:
   pass
+
+add_properties(A)
+
+
+
+if __name__ == '__main__':
+  a = A()
+  print a.a, a.b, a.c
