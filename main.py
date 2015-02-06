f8b48fadc65e6b9f61de6252d18cb02d95ae7e24
Doni Leung <doni@kudelabs.com>
2015-02-07 00:08:27 +0800

    Finish.

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python
 
+class A(object):
+    def method1(self):
+        print "A.method1"
+
+old_method = A.method1
+def new_method(self):
+    print 'new_method'
+    return old_method(self)
+
+A.method1 = new_method
 if __name__ == '__main__':
-    pass
+    A().method1()
