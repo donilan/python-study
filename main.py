f24f50854d3d954392a3a6e1befffc80fbdd2c5c
Doni Leung <doni@kudelabs.com>
2015-02-07 00:03:25 +0800

    Finish test.

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python
 
+class A(object):
+    def __init__(self):
+        print 'A'
+
+class B(A):
+    def __init__(self):
+        print 'B'
+        super(B, self).__init__()
+
+class C(A):
+    def __init__(self):
+        print 'C'
+        super(C, self).__init__()
+
+class D(C, B):
+    def __init__(self):
+        print 'D'
+        super(D, self).__init__()
+
+
 if __name__ == '__main__':
-  pass
+    D()
